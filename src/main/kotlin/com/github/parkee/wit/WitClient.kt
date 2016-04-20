5eea8ec339fc4306f02337f69ea541552b1f2902
Pavlo Klivak <parkeex@gmail.com>
2016-04-20 18:38:34 +0300

    add auth and api version request headers

--- src/main/kotlin/com/github/parkee/wit/WitClient.kt
+++ src/main/kotlin/com/github/parkee/wit/WitClient.kt
@@ -4,8 +4,9 @@ package com.github.parkee.wit
  * Created by parkee on 4/20/16.
  */
 
-class WitClient(val accessToken: String) {
+class WitClient(val accessToken: String, val acceptHeader: String = "application/vnd.wit.20160330+json") {
 
+    val headers = mapOf("authorization" to "Bearer $accessToken", "accept" to acceptHeader)
 
 
 }
